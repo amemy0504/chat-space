e57079a20f4c0228f9c9cbe2a2bb50f9dcb99f05
amemy0504 <amemy050401@gmail.com>
2020-01-28 13:33:39 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -98,6 +98,8 @@ $(function(){
         //メッセージが入ったHTMLに、入れ物ごと追加
         $('.messages').append(insertHTML);
         $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
     }
     })
     .fail(function() {
